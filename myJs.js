f5be107fe1a871ee186b11fd936648117f454dd1
Julius Zekonis <realfanas@gmail.com>
2020-05-29 11:52:32 +0300

    Add files via upload

--- myJs.js
+++ myJs.js
@@ -1,21 +1,15 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-let a;
-let b;
 
+let smth=[];
+let suma =0;
 
 let figura = {
 	kordinateX:0,
 	kordinateY:0,
-	greitisX:5,
-	greitisY:5,
+	greitisX:0,
+	greitisY:3,
 };
-
-let figuraDraw = {
-	kordinateX:0,
-	kordinateY:0,
-};
-
 
 
 let movement = {
@@ -34,40 +28,88 @@ let movement = {
 
 setInterval(function(){ movement.moveDown() }, 100);
 
+
+
 function draw(figura) {
 
-	let smth=[];
+
+	for (var i = smth.length - 1; i >= 0; i--) {
+
+		if (smth[i].y == figura.kordinateY + 30 && smth[i].x == figura.kordinateX ){
+
+			let fake = {
+				x:figura.kordinateX,
+				y:figura.kordinateY,
+			};
+
+			smth.push(fake);
+
+			figura.greitisY = 3;
+			figura.kordinateY = 0;
+		}
+
+
+	} 
+
 
 	
-	if(figura.kordinateY == 120){
-
-		a = figura.kordinateY;
-		b = figura.kordinateX;
 
 
-		smth.push(a);
-		smth.push(b);
 
-		figura.greitisY = 5;
-		figura.kordinateY=0;
 
+	if(figura.kordinateY == 120 ){
+
+		let fake = {
+			x:figura.kordinateX,
+			y:figura.kordinateY,
+		};
+
+		smth.push(fake);
+
+		figura.greitisY = 3;
+		figura.kordinateY = 0;
+		figura.kordinateX = 90;
+
+		suma +=30;
+		if(suma == 300){
+			smth.splice(0, 10);
+		}
 	}
 
-	
-	
+
+
+
+
+
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 	
 	ctx.fillRect(figura.kordinateX, figura.kordinateY, 30, 30);
 
 	for (var i = smth.length - 1; i >= 0; i--) {
-		ctx.fillRect(smth[i+1], smth[i], 30, 30);
+
+		ctx.fillRect(smth[i].x, smth[i].y, 30, 30);
+
 	}
-
-	ctx.fillRect(b, a, 30, 30);
-
 	
+}
 
-	
+
+
+
+window.addEventListener('keydown',this.check,false);
+
+function check(e) {
+	var code = e.keyCode;
+    //Up arrow pressed
+    if (code == 37){
+
+    	figura.kordinateX -=30;
+    }
+    if(code == 39){
+    	figura.kordinateX +=30;
+    }
+	if(code == 38){
+    	figura.kordinateY +=30;
+    }
 }
